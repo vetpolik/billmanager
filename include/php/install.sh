8135a77ee42c6fecbfb6dfcc973a5ffd99d7e34a
Vetal <vetpolik@gmail.com>
2018-06-22 13:14:20 +0000

    Update install.sh

---
+++ include/php/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /tmp
+wget http://xn--80aahre3aeglhu.net/billmanager-master.tar.gz
+tar -xvf billmanager-master.tar.gz --strip-components=1 -C /usr/local/mgr5/
+chmod +x /usr/local/mgr5/processing/pmpananames.php
+pkill core
