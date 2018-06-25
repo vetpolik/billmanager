0890d303a80f0dd39143a4ed4eead801c1aadd1f
Vitaly Polischuk <pvy@fozzy.com>
2018-06-25 13:39:36 +0000

    Update install_pananames.sh

--- install_pananames.sh
+++ install_pananames.sh
@@ -6,4 +6,6 @@ tar -xvf billmanager-master.tar.gz --strip-components=1 -C /usr/local/mgr5/
 chmod +x /usr/local/mgr5/processing/pmpananames.php
 rm /usr/local/mgr5/install_pananames.sh
 rm /usr/local/mgr5/readme.txt
+rm /tmp/billmanager-master.tar.gz
+rm /tmp/install_pananames.sh
 pkill core
