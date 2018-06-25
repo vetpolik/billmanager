07cf9747a2fd9099bd81f6244ce0e32d9d1da726
Vitaly Polischuk <pvy@fozzy.com>
2018-06-25 09:28:30 +0000

    Update install_pananames.sh

--- install_pananames.sh
+++ install_pananames.sh
@@ -4,4 +4,6 @@ yum -y install php php-mysqli
 wget http://xn--80aahre3aeglhu.net/billmanager-master.tar.gz
 tar -xvf billmanager-master.tar.gz --strip-components=1 -C /usr/local/mgr5/
 chmod +x /usr/local/mgr5/processing/pmpananames.php
+rm /usr/local/mgr5/install_pananames.sh
+rm /usr/local/mgr5/readme.txt
 pkill core
