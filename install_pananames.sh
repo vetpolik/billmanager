0b4f002af182c83955e7b4c95000ba1d53f45d0f
vetpolik <pulsar@i.ua>
2018-06-26 17:49:39 +0300

    Update install_pananames.sh

--- install_pananames.sh
+++ install_pananames.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 yum -y install php php-mysqli
-wget http://xn--80aahre3aeglhu.net/billmanager-master.tar.gz
-tar -xvf billmanager-master.tar.gz --strip-components=1 -C /usr/local/mgr5/
+wget https://codeload.github.com/Pananames/billmanager/zip/master
+tar -xvf billmanager-master.zip --strip-components=1 -C /usr/local/mgr5/
 chmod +x /usr/local/mgr5/processing/pmpananames.php
 rm /usr/local/mgr5/install_pananames.sh
-rm /usr/local/mgr5/readme.txt
-rm /tmp/billmanager-master.tar.gz
+rm /usr/local/mgr5/README.md
+rm /tmp/billmanager-master.zip
 rm /tmp/install_pananames.sh
 pkill core
